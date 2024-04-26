4a70bcf5e60ed8424a1e004b9498262468c97798
munjieun <mjsu10@naver.com>
2024-04-26 14:47:36 +0900

    employee 클래스 생성

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -5,5 +5,25 @@ package employee;
  * 4/25/24 description    :
  */
 public class Employee {
+    private String employeeName;
+    private String employeeId;
+    private Job job;
 
+    public Employee(String employeeName, String employeeId, Job job) {
+        this.employeeName = employeeName;
+        this.employeeId = employeeId;
+        this.job = job;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public Job getJob() {
+        return job;
+    }
 }
