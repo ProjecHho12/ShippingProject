54cc3b92db0f12b071ca76dc1d2e2d0f7d537f7c
binwoojoo <161430796+binwoojoo@users.noreply.github.com>
2024-04-26 17:47:38 +0900

    # Customer 로그인 기능

--- src/customer/CustomerRepository.java
+++ src/customer/CustomerRepository.java
@@ -1,14 +1,34 @@
 package customer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerRepository {
 
-
-    CustomerList customers;
+    private List<Customer> customers;
 
     public CustomerRepository() {
-
-        this.customers = new CustomerList();
-
+        this.customers = new ArrayList<>();
     }
 
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public void addNewCustomer(Customer newCustomer) {
+        customers.add(newCustomer);
+    }
+
+    public boolean checkId(String id) {
+        // 포문 돌려서 찾기
+        return false;
+    }
+
+    public void checkPassword(String password) {
+
+    }
 }
