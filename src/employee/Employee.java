7a8503173434e69440084637406155abb1855a73
MunJiEun <94054978+munmimi@users.noreply.github.com>
2024-04-29 14:25:19 +0900

    employees.txt 추가 (#10)
    * employees.txt 추가
    
    * Delete src/employee/employees.txt
    
    ---------
    
    Co-authored-by: munjieun <mjsu10@naver.com>
    Co-authored-by: hoho <62678380+hoseong511@users.noreply.github.com>

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -6,12 +6,17 @@ package employee;
  */
 public class Employee {
     private String employeeName;
-    private String employeeId;
+    private int employeePhone;
+    private String employeeID;
+    private String employeePW;
     private Job job;
 
-    public Employee(String employeeName, String employeeId, Job job) {
+
+    public Employee(String employeeName, int employeePhone, String employeeID, String employeePW, Job job) {
         this.employeeName = employeeName;
-        this.employeeId = employeeId;
+        this.employeePhone = employeePhone;
+        this.employeeID = employeeID;
+        this.employeePW = employeePW;
         this.job = job;
     }
 
@@ -19,11 +24,49 @@ public class Employee {
         return employeeName;
     }
 
-    public String getEmployeeId() {
-        return employeeId;
+    public int getEmployeePhone() {
+        return employeePhone;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getEmployeePW() {
+        return employeePW;
     }
 
     public Job getJob() {
         return job;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public void setEmployeePhone(int employeePhone) {
+        this.employeePhone = employeePhone;
+    }
+
+    public void setEmployeePW(String employeePW) {
+        this.employeePW = employeePW;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeName='" + employeeName + '\'' +
+                ", employeeID='" + employeeID + '\'' +
+                ", employeePW='" + employeePW + '\'' +
+                ", job=" + job +
+                '}';
     }
 }
