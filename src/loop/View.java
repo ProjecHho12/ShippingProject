042c24bd8c465228e35a88db5c2723c8e2456123
hoho <62678380+hoseong511@users.noreply.github.com>
2024-04-29 14:23:58 +0900

    [PR] feature event loop and main select view (#11)
    * WIP loop(1)
    
    * feat mainView
    
    - 직원 전용 화면
    - 고객 전용 화면
    - 직원/고객 등록 화면

---
+++ src/loop/View.java
@@ -0,0 +1,9 @@
+package loop;
+
+/**
+ * packageName    : loop fileName       : View author         : hoho date           : 4/26/24
+ * description    :
+ */
+public interface View {
+	void run();
+}
