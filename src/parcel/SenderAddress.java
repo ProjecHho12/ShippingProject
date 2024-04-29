37022e1a8c36397057215055135e287ce8a080af
yunjeongg <bae.yunjeongg@gmail.com>
2024-04-30 08:42:39 +0900

    # Parcel 운송장번호 추가, Parcel 입력내용 등록, 조회 추가

---
+++ src/parcel/SenderAddress.java
@@ -0,0 +1,39 @@
+package parcel;
+
+import parcel.Tr.TrackingNumber;
+
+public class SenderAddress {
+    private TrackingNumber SenderAddressStateProvinceRegion; // 광역시,도
+    private String SenderAddressCity; // 시/,군,구
+    private String SenderAddressStreetAddress; // 세부주소
+    private String SenderAddressZipPostalCode; // 우편번호
+
+    public SenderAddress(TrackingNumber senderAddressStateProvinceRegion, String senderAddressCity, String senderAddressStreetAddress, String senderAddressZipPostalCode) {
+        SenderAddressStateProvinceRegion = senderAddressStateProvinceRegion;
+        SenderAddressCity = senderAddressCity;
+        SenderAddressStreetAddress = senderAddressStreetAddress;
+        SenderAddressZipPostalCode = senderAddressZipPostalCode;
+    }
+
+    public String getSenderAddressZipPostalCode() {
+        return SenderAddressZipPostalCode;
+    }
+
+    public String getSenderAddressStreetAddress() {
+        return SenderAddressStreetAddress;
+    }
+
+    public String getSenderAddressCity() {
+        return SenderAddressCity;
+    }
+
+    public TrackingNumber getSenderAddressStateProvinceRegion() {
+        return SenderAddressStateProvinceRegion;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("광역시/도: %s, 시/군/구: %s, 세부주소: %s, 우편번호: %s",
+                this.SenderAddressStateProvinceRegion, this.SenderAddressCity, this.SenderAddressStreetAddress, this.SenderAddressZipPostalCode);
+    }
+}
---
+++ src/parcel/Tr/TrackingNumber.java
@@ -0,0 +1,42 @@
+package parcel.Tr;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum TrackingNumber {
+    // 운송장 번호 열거형
+    SEOUL("002", Arrays.asList("SEOUL", "서울")),
+    INCHON("032", Arrays.asList("INCHON", "인천")),
+    DAEJEON("042", Arrays.asList("DAEJEON", "대전")),
+    BUSAN("051", Arrays.asList("BUSAN", "부산")),
+    ULSAN("052", Arrays.asList("ULSAN", "울산")),
+    DAEGU("053", Arrays.asList("DAEGU", "대구")),
+    GWANGJU("062", Arrays.asList("GWANGJU", "광주")),
+    JEJU("064", Arrays.asList("JEJU", "제주도", "제주")),
+    GYEONGGI("031", Arrays.asList("GYEONGGI", "경기도", "경기")),
+    GANGWON("033", Arrays.asList("GANGWON", "강원도", "강원")),
+    CHUNGNAM("041", Arrays.asList("CHUNGNAM", "충청남도","충남")),
+    CHUNGBUK("043", Arrays.asList("CHUNGBUK", "충청북도","충북")),
+    GYEONGBUK("054", Arrays.asList("GYEONGBUK", "경상북도", "경북")),
+    GYEONGNAM("055", Arrays.asList("GYEONGNAM", "경상남도", "경남")),
+    JEONNAM("061", Arrays.asList("JEONNAM", "전라남도", "전남")),
+    JEONBUK("063", Arrays.asList("JEONBUK", "전라북도", "전북"));
+
+    private String code;
+    private final List<String> names;
+
+    TrackingNumber(String code, List<String> names) {
+        this.code = code;
+        this.names = names;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+
+}
