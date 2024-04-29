f7e2752fc5162775cd822ba5a91a56425feb5d59
munjieun <mjsu10@naver.com>
2024-04-29 18:15:25 +0900

    emplotee.txt 로드 수정

--- src/employee/EmployeeRepository.java
+++ src/employee/EmployeeRepository.java
@@ -1,22 +1,65 @@
 package employee;
 
+import customer.Gender;
 import loop.Repository;
 import parcel.Parcel;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class EmployeeRepository implements Repository {
 
-    EmployeeList employeeList;
+    public List<Employee> employeeList;
 
+    String targetPath = "/Users/jieun/Desktop/teamProject/javaProject/Shipping/src/employee/employees.txt";
 
     public EmployeeRepository() {
-        this.employeeList = new EmployeeList();
+
+        this.employeeList = new ArrayList<>();
+
+
+        File file = new File(targetPath);
+        if (!file.exists()) return;
+
+        try (FileReader fr = new FileReader(targetPath)) {
+            BufferedReader br = new BufferedReader(fr);
+
+
+            while (true) {
+                String s = br.readLine();
+
+
+                if (s == null) break;
+
+                String[] split = s.split(",");
+                System.out.println(Arrays.toString(split));
+
+                Job job = null;
+                if (split[7].equals(Job.OUTCOMING.name())) job = Job.OUTCOMING;
+                else if (split[7].equals(Job.INCOMING.name())) job = Job.INCOMING;
+                // 읽어들인 회원정보로 회원 객체 생성
+                Employee employee = new Employee(
+                        split[0],
+                        split[1],
+                        split[2],
+                        split[3],
+                        split[4],
+                        Integer.parseInt(split[5]),
+                        Integer.parseInt(split[6]),
+                        job
+                );
+
+                this.employeeList.add(employee);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     /**
@@ -26,12 +69,13 @@ public class EmployeeRepository implements Repository {
      * @return 입력받은 정보를 가진 직원이 있으면 true
      */
     public boolean isContains(Employee target) {
-        return employeeList.employeeList.contains(target);
+        return employeeList.contains(target);
     }
 
     /**
      * 입력 받은 정보로 새 직원 생성
-     * @param name 새 직원 이름
+     *
+     * @param name          새 직원 이름
      * @param email
      * @param password
      * @param gender
@@ -40,22 +84,45 @@ public class EmployeeRepository implements Repository {
      * @param employeePhone
      * @param job
      */
-    public void resister(String name, String email, String password,String gender, String address, int age, int employeePhone, Job job) {
-        Employee newEmployee = new Employee(name, email, password, gender, address, age, employeePhone,  job);
+    public void resister(String name, String email, String password, String gender, String address, int age, int employeePhone, Job job) {
+        Employee newEmployee = new Employee(name, email, password, gender, address, age, employeePhone, job);
         if (!isContains(newEmployee)) {
-            employeeList.addEmployee(newEmployee);
+            employeeList.add(newEmployee);
+
+
+            try (FileWriter fw = new FileWriter(targetPath, true)) {
+
+                String column = String.format(newEmployee.getEmployeeName() + ',' + newEmployee.getEmployeeEmail() + ',' + newEmployee.getEmployeePW() + ',' + newEmployee.getEmployeeGender() + ',' + newEmployee.getEmployeeAddress() + ',' + newEmployee.getAge() + ',' + newEmployee.getEmployeePhone() + ',' + newEmployee.getJob());
+                fw.write(column + "\n");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
         }
     }
 
 
     public void printEmployee() {
-        int i = 0;
-        for (Employee em : employeeList.getEmployeeList()) {
-            i++;
-            System.out.println(i + ". " + em);
-        }
+
     }
 
+
+    public Employee login(String email, String password) {
+//        System.out.println(employeeList.getEmployeeList());
+        Employee matechedEmployee = employeeList.stream().filter(e -> e.getEmployeeEmail().equals(email)).collect(Collectors.toList()).get(0);
+
+        if (matechedEmployee != null) {
+            if (matechedEmployee.getEmployeePW() == password) {
+
+                return matechedEmployee;
+            }
+        }
+        return null;
+
+
+    }
 
     List<Parcel> parcelList = new ArrayList<>();
 
@@ -67,28 +134,13 @@ public class EmployeeRepository implements Repository {
         }
     }
 
-    public Employee login (String email, String password){
-        Employee matechedEmployee = employeeList.getEmployeeList().stream().filter(e->e.getEmployeeEmail().equals(email)).collect(Collectors.toList()).get(0);
-
-       if (matechedEmployee != null){
-           if (matechedEmployee.getEmployeePW() == password){
-
-               return matechedEmployee;
-           }
-       }
-       return null;
-
+    public void incomeParcel() {
 
     }
 
-    public void incomeParcel(){
+    public void outcomeParcel() {
 
     }
-    public void outcomeParcel(){
-
-    }
-
-
 
 
 }
--- src/employee/Main.java
+++ src/employee/Main.java
@@ -1,39 +1,26 @@
 package employee;
 
 import java.io.*;
+import java.text.Format;
 
 public class Main {
     public static void main(String[] args) {
         EmployeeRepository repository = new EmployeeRepository();
-        String ROOT_PATH = "./src/employee/";
         try {
-            repository.resister("mmm","mmm@mmm.mmm","mmm","f", "mmm", 10,01012341234,Job.INCOMING);
-//            repository.resister("mmm",90789098,"mmm","mmm", Job.INCOMING);
-            repository.printEmployee();
-            repository.login("mmm","mmm");
+//            repository.resister("mmm", "mmm@mmm.mmm", "mmm", "f", "mmm", 10, 01012341234, Job.INCOMING);
+//            repository.resister("fff", "fff@fff.fff", "fff", "f", "mmm", 10, 01012341234, Job.INCOMING);
+//            repository.resister("zz", "zz@fff.fff", "zz", "f", "mmm", 10, 01012341234, Job.OUTCOMING);
+//            repository.printEmployee();
+//           Employee matechedEmployee = repository.login("mmm", "mmm");
+//            System.out.println("matechedEmployee = " + matechedEmployee);
 
+            System.out.println(repository.employeeList);
         } catch (Exception e) {
             e.printStackTrace();
-        };
-
-        // 파일 생성하기
-        File newFile1 = new File(ROOT_PATH + "employees.txt");
-        if (!newFile1.exists()) {
-            try {
-                newFile1.createNewFile();
-                for (Employee em : repository.employeeList.getEmployeeList()){
-
-                }
-            } catch (IOException e) {
-                System.out.println("파일 생성에 실패했습니다.");
-            }
         }
+        ;
 
-        try (FileWriter fw = new FileWriter(ROOT_PATH +  "employees.txt")){
-            // 파일 생성 명령
-            for (Employee em:repository.employeeList.getEmployeeList()){
-                fw.write(em + "\n");
-            }
-        } catch(Exception e){}
+
+
     }
 }
