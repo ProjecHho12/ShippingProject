7249ef7b0b10da036f7b92298feec832149784ed
oooowb <oooo940212@gmail.com>
2024-05-03 15:47:04 +0900

    고객 택배 조회

--- src/parcel/Main.java
+++ src/parcel/Main.java
@@ -5,12 +5,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) {
-
-        ParcelView parcelView = new ParcelView();
-
-        parcelView.pacelrun();
-
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        ParcelView parcelView = new ParcelView();
+//
+//        parcelView.pacelrun();
+//
+//    }
+//}
