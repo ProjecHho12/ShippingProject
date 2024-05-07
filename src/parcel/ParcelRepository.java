e3022d77823147d21cc6ea8f6b3c9f5b45a6ed32
hoseong511 <thdghtjd115@naver.com>
2024-05-07 14:10:29 +0900

    fix dup method name

--- src/parcel/ParcelRepository.java
+++ src/parcel/ParcelRepository.java
@@ -20,9 +20,6 @@ public class ParcelRepository implements Serializable {
     public ParcelRepository(TrackingNumber[] trackingNumbers) {
     }
 
-    public ParcelRepository(TrackingNumber[] trackingNumbers) {
-    }
-
     public static ParcelRepository getInstance() {
         if (instance == null) {
             instance = new ParcelRepository ();
