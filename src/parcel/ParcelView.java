0dc00ab7f3d33cc224173a5d79e7c8ab9ee3f89d
oooowb <oooo940212@gmail.com>
2024-05-07 10:29:25 +0900

    customer fix

--- src/parcel/ParcelView.java
+++ src/parcel/ParcelView.java
@@ -1,8 +1,11 @@
 package parcel;
 
+import customer.Customer;
 import parcel.parcelElement.*;
 
 import java.io.Serializable;
+
+import static parcel.parcelElement.InformationCheck.si;
 
 public class ParcelView implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -19,14 +22,11 @@ public class ParcelView implements Serializable {
     }
 
     // 택배 접수하기 (택배 리스트 & 외부저장소에 택배 1개 저장)
-    public void receiptParcel(Sender sender, Recipient recipient, Parcel parcel, ProductInfo productInfo) {
+    public void receiptParcel(Sender sender, Recipient recipient, Parcel parcel, ProductInfo productInfo, Customer tar) {
         // 입력된 정보 바탕으로 택배 1개 초기화
 //        this.parcel.setSender(sender);
 //        this.parcel.setRecipient(recipient);
         this.parcel.setProductInfo(productInfo);
-
-
-
 
 
         // 택배리스트 에 택배 저장하기
@@ -46,10 +46,11 @@ public class ParcelView implements Serializable {
         // 여기 다른분 꺼 내용지운거 추가해야됨 ㄴㅇ모ㅗㅓㅑㅐㅅ거ㅙㅈㅅㄱ너ㅐ조거ㅐ
         // 여기도 다른분꺼 내용지운거 추가하기 리ㅑ허마ㅐ가ㅔㅗㄴ기ㅓㅛㅔㅓㅏㅐ둇아ㅓ; 기존 startInputParcel메소드
         //System.out.println(Arrays.toString(repository.getParcelArrayList().toArray())); // 이거오류
+        tar.addTrackingNumbers(parcel.getTrackingNumber());
     }
 
     // 실행메소드 구현하기
-    public void start() {
+    public void start(Customer tar) {
 
         // 보내는 사람
         Sender sender = new Sender(si);
@@ -74,7 +75,7 @@ public class ParcelView implements Serializable {
         productInfo.checkInfo(); // 입력 메서드 실행 및 객체 생성
 
         //System.out.println("죄회죄화" + productInfo);
-        receiptParcel(sender, recipient, parcel1, productInfo);
+        receiptParcel(sender, recipient, parcel1, productInfo, tar);
 
 
         // 접수내용 조회
@@ -95,20 +96,20 @@ public class ParcelView implements Serializable {
     }
 
 
-    // 택배리스트 전체조회
-    public void showAllParcelList() {
-        System.out.printf("현재 접수된 택배\uD83D\uDCE6 목록입니다. \n 총 %d개가 접수완료 상태입니다.\n\n", repository.getParcelArrayList().size());
-        // 단축키 iter
-
-        System.out.println("파일에서 읽어온 택배 리스트:");
-        for (Parcel parcel : repository.getParcelArrayList()) {
-            System.out.println("접수일자   : " + parcel.getRegDate());
-            System.out.println("운송장번호 : " + parcel.getTrackingNumber());
-            System.out.println("parcel.getSender()");
-            System.out.println("parcel.getRecipient()");
-            System.out.println("상품 정보  : " + parcel.getProductInfo());
-            System.out.println("택배 상태  : " + parcel.getStatus());
-        }
+//    // 택배리스트 전체조회
+//    public void showAllParcelList() {
+//        System.out.printf("현재 접수된 택배\uD83D\uDCE6 목록입니다. \n 총 %d개가 접수완료 상태입니다.\n\n", repository.getParcelArrayList().size());
+//        // 단축키 iter
+//
+//        System.out.println("파일에서 읽어온 택배 리스트:");
+//        for (Parcel parcel : repository.getParcelArrayList()) {
+//            System.out.println("접수일자   : " + parcel.getRegDate());
+//            System.out.println("운송장번호 : " + parcel.getTrackingNumber());
+//            System.out.println("parcel.getSender()");
+//            System.out.println("parcel.getRecipient()");
+//            System.out.println("상품 정보  : " + parcel.getProductInfo());
+//            System.out.println("택배 상태  : " + parcel.getStatus());
+//        }
 //        for (Parcel parcel : repository.getParcelArrayList()) {
 //            System.out.println(parcel);
 //        }
@@ -121,35 +122,33 @@ public class ParcelView implements Serializable {
 //                System.out.println(parcel);
 //            }
 //        }
-    }
 
-    // 4. 프로그램 종료 메서드
-    public Boolean exitProgram() {
-        while (true) {
-            String selectExit = si.inputString("프로그램을 종료하시겠습니까? True / False");
-
-            if (selectExit.toUpperCase().contains("T")) {
-                repository.closeProgram();
-                return true;
-            } else if (selectExit.toUpperCase().contains("F")) {
-                return false;
-            } else {
-                System.out.println("True 또는 False 를 입력해 주세요 ❗");
-            }
-        }
-    }
-
-    // 0. 메뉴조회 및 사용할 메뉴 선택
-    public String showMenu() {
-        System.out.println("\n* ------- 택배 등록\uD83D\uDCE6 ------- *");
-        System.out.println("1. 택배 등록");
-        System.out.println("2. 택배 조회");
-        //System.out.println("3. 택배 운송");
-        System.out.println("4. 프로그램 종료\uD83D\uDE25");
-        System.out.println("* -------------------------- *");
-
-        return si.inputString("이용하실 메뉴번호\uD83D\uDD22를 입력해주세요 ❗");
-    }
-
-
+//    // 4. 프로그램 종료 메서드
+//    public Boolean exitProgram() {
+//        while (true) {
+//            String selectExit = si.inputString("프로그램을 종료하시겠습니까? True / False");
+//
+//            if (selectExit.toUpperCase().contains("T")) {
+//                repository.closeProgram();
+//                return true;
+//            } else if (selectExit.toUpperCase().contains("F")) {
+//                return false;
+//            } else {
+//                System.out.println("True 또는 False 를 입력해 주세요 ❗");
+//            }
+//        }
+//    }
 }
+//    // 0. 메뉴조회 및 사용할 메뉴 선택
+//    public String showMenu() {
+//        System.out.println("\n* ------- 택배 등록\uD83D\uDCE6 ------- *");
+//        System.out.println("1. 택배 등록");
+//        System.out.println("2. 택배 조회");
+//        //System.out.println("3. 택배 운송");
+//        System.out.println("4. 프로그램 종료\uD83D\uDE25");
+//        System.out.println("* -------------------------- *");
+//
+//        return si.inputString("이용하실 메뉴번호\uD83D\uDD22를 입력해주세요 ❗");
+//    }
+
+
